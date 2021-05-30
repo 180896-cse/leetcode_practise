22d1eecdeb2f68d54278cf622d03d785256ae9dc
Shantanu <shantanupratap180896@gmail.com>
2021-05-31 00:31:11 +0530

    brute-force approach

--- Majority_elements_leetcode.cpp
+++ Majority_elements_leetcode.cpp
@@ -3,33 +3,28 @@
 #include<math.h>
 using namespace std;
 
+                                         // BRUTE-FORCE APPROACH---O(n^2);
+
+
 void Brute_majority(vector<int>v){
 int s=(v.size()+1)/2;
 auto itr=max_element(v.begin(),v.end());
-map<int,int>mp;
-map<int,int>::iterator im;
-multimap<int,int>mtmp;
-multimap<int,int>::iterator iy;
-
+multimap<int,int>mp;
+multimap<int,int>::iterator it;
 for(int i=0;i<v.size();i++){
     int cnt=count(v.begin(),v.end(),v[i]);
-    auto it=mp.find(v[i]);
-    if(it == mp.end()){
-        mp.insert(make_pair(v[i],cnt));
-        
-        
-
+    mp.insert(make_pair(cnt,v[i]));
+}
+for(auto & elm:mp){
+    if(elm.first>=s){
+        cout<<elm.second;
+        break;
     }
-    
-    }
-
-for(im=mp.begin();im<mp.end();im++){
-    
 }
 
-for(auto value:mtmp){
-    cout<<value.first<<" "<<value.second<<" "<<" ";
-}
+
+
+
 
 
 
@@ -49,6 +44,9 @@ int main(){
 vector<int>v={-1,2,2,1,2,1};
 
 Brute_majority(v);
+
+cout<<"\n";
+
 
 
     return 0;
