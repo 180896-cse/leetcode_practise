decde748f39ed004590f93a8f1cec2c1d9cefd58
Shantanu <shantanupratap180896@gmail.com>
2021-05-31 22:16:34 +0530

    Brute-force approach

--- Majority_elements_leetcode--II.cpp
+++ Majority_elements_leetcode--II.cpp
@@ -2,11 +2,41 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
+
+
+                                                // BRUTE--FORCE APPROACH---O(n^3) AND SPACE COMPLEXITY-- O(n^2);
+
+vector<int>brute_majority(vector<int>nums){
+    
+vector<int>k;        
+int knt=0;
+        int s=(nums.size()/3);
+            
+        for(int i=0;i<nums.size();i++){
+            int knt=count(nums.begin(),nums.end(),nums[i]);
+            auto it= find(k.begin(),k.end(),nums[i]);
+            if(knt>s && it==k.end()){
+                 k.push_back(nums[i]);
+            }
+            
+            
+            
+            
+        }
+return k;
+}
+
+
+
+
+
 int main(){
+vector<int>nums={1,2};
 
-
-
-
+vector<int>s=brute_majority(nums);
+for(auto & i:s){
+    cout<<i<<" ";
+}
 
 
 
