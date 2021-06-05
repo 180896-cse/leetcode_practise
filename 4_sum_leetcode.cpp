efe7a240e7c5ac61a86b6d8ff1d45cc2ac57ba21
Shantanu <shantanupratap180896@gmail.com>
2021-06-05 23:51:13 +0530

    brute-->processing

---
+++ 4_sum_leetcode.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+int main(){
+
+vector<int>v={1,0,-1,0,-2,2};
+for(int i=0;i!=v.size()-4;i++){
+cout<<v[i]<<" ";
+}
+
+
+
+
+
+return 0; 
+
+}
